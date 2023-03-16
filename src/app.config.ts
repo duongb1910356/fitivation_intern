89fe2ec32727db71a48ef8e67d359ee2d5ce6777
Raymond <tvngoan@drimaes.com>
2023-03-16 17:08:17 +0700

    handle upload files

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,4 +5,5 @@ export const appConfig = {
   jwtExpiresIn: '60s',
   jwtSecret: process.env.JWT_SECRET,
   fileHost: process.env.FILE_HOST,
+  fileRoot: process.env.FILE_ROOT,
 };
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,5 +1,26 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { mkdirSync, writeFileSync } from 'fs';
+import { appConfig } from 'src/app.config';
+import { AvatarUploadDto } from './dto/avatar-upload-dto';
 import { CreateUserDto } from './dto/create-user-dto';
 import { User } from './schemas/user.schema';
 import { UsersService } from './users.service';
@@ -10,6 +31,7 @@ export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get(':id')
+  @ApiParam({ name: 'id', type: String })
   @ApiResponse({ type: User, status: 200 })
   getUserById(@Param('id') id) {
     return this.userService.findOne({ _id: id });
@@ -20,5 +42,30 @@ export class UsersController {
   @ApiResponse({ type: User, status: 201 })
   createUser(@Body() input: CreateUserDto) {
     return this.userService.createOne(input);
+  }
+
+  @Post(':id/avatar')
+  @UseInterceptors(FileInterceptor('avatar'))
+  @ApiConsumes('multipart/form-data')
+  @ApiParam({ name: 'id', type: String })
+  @ApiBody({ type: AvatarUploadDto })
+  uploadFile(
+    @Param('id') id,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1000 * 1000 }), // 1MB
+          new FileTypeValidator({ fileType: /(?:jpeg|png)/i }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    const dir = `${appConfig.fileRoot}/${id}`;
+    mkdirSync(dir, { recursive: true });
+    writeFileSync(`${dir}/${file.originalname}`, file.buffer);
+    return {
+      url: appConfig.fileHost + `/${id}/${file.originalname}`,
+    };
   }
 }
