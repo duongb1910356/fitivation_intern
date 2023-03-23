8aba0d225ed94a7b572cb0e27eee10a3c35263d0
Cristian <tdkhang@drimaes.com>
2023-03-23 14:42:37 +0700

    rename folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { appConfig } from './app.config';
-import { AppLoggerMiddleware } from './common/middlewares/logging.middleware';
+import { AppLoggerMiddleware } from './common/middleware/logging.middleware';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
 import { UsersModule } from './modules/users/users.module';
