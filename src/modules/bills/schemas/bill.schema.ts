b2664788ec2798dd3adca17a8410c5635015913b
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-05-26 10:35:54 +0700

    fix(bill schema): add & fix validate property

--- src/modules/bills/schemas/bill.schema.ts
+++ src/modules/bills/schemas/bill.schema.ts
@@ -1,4 +1,4 @@
-import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { BaseObject } from 'src/shared/schemas/base-object.schema';
 import { User } from 'src/modules/users/schemas/user.schema';
@@ -47,7 +47,7 @@ export class Bill extends BaseObject {
 	@Prop({ required: true, enum: PaymentMethod, type: String })
 	paymentMethod: PaymentMethod;
 
-	@Prop({ type: Number, default: 0 })
+	@Prop({ type: Number, default: 0, min: 0 })
 	taxes?: number;
 
 	@Prop({ required: true, type: Number, min: 0 })
@@ -56,7 +56,7 @@ export class Bill extends BaseObject {
 	@Prop({ default: Status.ACTIVE, enum: Status, type: String })
 	status: Status;
 
-	@Prop({ type: String, minlength: 0, maxlength: 30 })
+	@Prop({ type: String, minlength: 0, maxlength: 200 })
 	description?: string;
 
 	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Promotion' })
