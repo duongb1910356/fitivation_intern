777607f145e8ac16f507a6a40aa3e2dae60a4c19
DustHuynh <huynhdust0411@gmail.com>
2023-05-26 09:37:01 +0700

    remove facilityID in ShiftTime

--- src/modules/facility-schedule/entities/shift-time.entity.ts
+++ src/modules/facility-schedule/entities/shift-time.entity.ts
@@ -1,15 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
 
 @Schema()
 export class ShiftTime {
-	@Prop({
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Facility',
-		required: true,
-	})
-	facilityID: string; //Faccility
-
 	@Prop({ type: Date })
 	startTime: Date;
 
