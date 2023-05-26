c10ed8592bcd9728d703b2d9e9967fdd526c2a8c
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-05-26 10:07:15 +0700

    fix(cart schema): remove ApiProperty decorator

--- src/modules/carts/schemas/cart.schema.ts
+++ src/modules/carts/schemas/cart.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-import { ApiProperty } from '@nestjs/swagger';
 import { BaseObject } from 'src/shared/schemas/base-object.schema';
 import { User } from 'src/modules/users/schemas/user.schema';
 import { Promotion } from 'src/modules/promotions/schemas/promotion.schema';
@@ -10,11 +9,9 @@ export type CartDocument = HydratedDocument<Cart>;
 @Schema({ timestamps: true })
 export class Cart extends BaseObject {
 	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
-	@ApiProperty()
 	accountID: User;
 
 	@Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Promotion' }] })
-	@ApiProperty()
 	promotions?: Promotion[];
 }
 
