e213021d12f1a27a849cefac5f4a208c845b2403
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-05-29 10:53:59 +0700

    fix(cart schema): add property

--- src/modules/carts/schemas/cart.schema.ts
+++ src/modules/carts/schemas/cart.schema.ts
@@ -3,6 +3,7 @@ import mongoose, { HydratedDocument } from 'mongoose';
 import { BaseObject } from 'src/shared/schemas/base-object.schema';
 import { User } from 'src/modules/users/schemas/user.schema';
 import { Promotion } from 'src/modules/promotions/schemas/promotion.schema';
+import { CartItem } from './cart-item.schema';
 
 export type CartDocument = HydratedDocument<Cart>;
 
@@ -11,8 +12,17 @@ export class Cart extends BaseObject {
 	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
 	accountID: User;
 
+	@Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'CartItem' }] })
+	cartItems: CartItem[];
+
 	@Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Promotion' }] })
 	promotions?: Promotion[];
+
+	@Prop({ default: 0, type: Number, min: 0 })
+	promotionPrice: number;
+
+	@Prop({ required: true, type: Number, min: 0 })
+	totalPrice: number;
 }
 
 export const CartSchema = SchemaFactory.createForClass(Cart);
