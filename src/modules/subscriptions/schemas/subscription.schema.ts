3c27bcda1271059527076f9434b7b7772e5c5c01
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-01 15:11:34 +0700

    fix(subscription schema): fix Bill-Item schema path

--- src/modules/subscriptions/schemas/subscription.schema.ts
+++ src/modules/subscriptions/schemas/subscription.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { BaseObject } from 'src/shared/schemas/base-object.schema';
 import { User } from 'src/modules/users/schemas/user.schema';
-import { BillItem } from 'src/modules/bills/schemas/bill-item.schema';
+import { BillItem } from 'src/modules/bill-items/schemas/bill-item.schema';
 import { Package } from 'src/modules/package/entities/package.entity';
 
 export type SubscriptionDocument = HydratedDocument<Subscription>;
