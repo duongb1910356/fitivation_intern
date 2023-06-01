7bd5b2b71c78766168f1aa44275129ae10365d40
DustHuynh <huynhdust0411@gmail.com>
2023-06-01 15:17:47 +0700

    fix: git add all

--- src/decorators/requestWithUser.interface.ts
+++
@@ -1,9 +0,0 @@
-import { User } from 'src/modules/users/schemas/user.schema';
-import { Request } from '@nestjs/common';
-
-interface RequestWithUser extends Request {
-	user: User;
-}
-
-export default RequestWithUser;
-//https://wanago.io/2020/05/25/api-nestjs-authenticating-users-bcrypt-passport-jwt-cookies/
--- src/decorators/role/requestWithUser.interface.ts
+++
@@ -1,8 +0,0 @@
-import { User } from 'src/modules/users/schemas/user.schema';
-import { Request } from '@nestjs/common';
-
-interface RequestWithUser extends Request {
-	user: User;
-}
-
-export default RequestWithUser;
