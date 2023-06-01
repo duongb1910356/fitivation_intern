c97c54235de887ca89d5c92a51c948f6dc4f4f05
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-01 15:14:46 +0700

    build(cart-items module): include schema & controller

---
+++ src/modules/cart-items/cart-items.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { CartItemsController } from './cart-items.controller';
+
+@Module({
+  controllers: [CartItemsController]
+})
+export class CartItemsModule {}
