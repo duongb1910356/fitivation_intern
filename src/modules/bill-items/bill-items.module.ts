916fd3e67d482931235eddfbec37c3b6fa7e5983
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-04 11:22:06 +0700

    build(bill-item module): add service files

--- src/modules/bill-items/bill-items.module.ts
+++ src/modules/bill-items/bill-items.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BillItemsController } from './bill-items.controller';
+import { BillItemsService } from './bill-items.service';
 
 @Module({
 	controllers: [BillItemsController],
+	providers: [BillItemsService],
 })
 export class BillItemsModule {}
