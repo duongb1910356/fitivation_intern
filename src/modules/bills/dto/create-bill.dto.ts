14ee0a4ec86d35cc079016c723387b95cbcad9b3
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-06 16:07:55 +0700

    fix(create bill dto): remove ApiProperty

--- src/modules/bills/dto/create-bill.dto.ts
+++ src/modules/bills/dto/create-bill.dto.ts
@@ -12,56 +12,46 @@ import {
 	MaxLength,
 	Min,
 } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 import { BillStatus, PaymentMethod } from '../schemas/bill.schema';
 
 export class CreateBillDto {
-	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	accountID: string;
 
-	@ApiProperty()
 	@IsArray()
 	@ArrayNotEmpty()
 	@ArrayMinSize(1)
 	billItems: object[];
 
-	@ApiProperty()
 	@IsEnum(PaymentMethod)
 	@IsNotEmpty()
 	paymentMethod: PaymentMethod;
 
-	@ApiProperty()
 	@IsNumber()
 	@Min(0)
 	taxes: number;
 
-	@ApiProperty()
 	@IsString()
 	@IsOptional()
 	@MinLength(2)
 	@MaxLength(200)
 	description?: string;
 
-	@ApiProperty()
 	@IsArray()
 	@IsOptional()
 	promotions?: object[];
 
-	@ApiProperty()
 	@IsNumber()
 	@IsNotEmpty()
 	@Min(0)
 	promotionsPrice: number;
 
-	@ApiProperty()
 	@IsNumber()
 	@IsNotEmpty()
 	@IsPositive()
 	totalPrice: number;
 
-	@ApiProperty()
 	@IsEnum(BillStatus)
 	@IsOptional()
 	status?: BillStatus;
