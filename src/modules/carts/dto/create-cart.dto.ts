7deb619319d177c4e6eb9cf279146ea5cd0478f4
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-06 11:13:15 +0700

    fix(create cart dto): remove cartItemIDs prop

--- src/modules/carts/dto/create-cart.dto.ts
+++ src/modules/carts/dto/create-cart.dto.ts
@@ -1,35 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
-  IsArray,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
+	IsArray,
+	IsNotEmpty,
+	IsNumber,
+	IsOptional,
+	IsString,
 } from 'class-validator';
 
 export class CreateCartDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  accountID: string;
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	accountID: string;
 
-  @ApiProperty()
-  @IsOptional()
-  @IsArray()
-  cartItemIDs?: object[];
+	@ApiProperty()
+	@IsOptional()
+	@IsArray()
+	promotionsIDs?: object[];
 
-  @ApiProperty()
-  @IsOptional()
-  @IsArray()
-  promotionsIDs?: object[];
+	@ApiProperty()
+	@IsNumber()
+	@IsOptional()
+	promotionPrice?: number;
 
-  @ApiProperty()
-  @IsNumber()
-  @IsOptional()
-  promotionPrice?: number;
-
-  @ApiProperty()
-  @IsNumber()
-  @IsOptional()
-  totalPrice?: number;
+	@ApiProperty()
+	@IsNumber()
+	@IsOptional()
+	totalPrice?: number;
 }
