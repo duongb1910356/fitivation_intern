bb7d3a0ce194c757c7a30a4d6bec12dc668f492c
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-06 11:33:19 +0700

    build(create-subscription dto): build prop & validation

--- src/modules/subscriptions/dto/create-subscription.dto.ts
+++ src/modules/subscriptions/dto/create-subscription.dto.ts
@@ -1,16 +1,33 @@
 import {
 	IsNotEmpty,
 	IsString,
-	IsNumber,
 	IsEnum,
-	IsArray,
-	IsOptional,
-	ArrayNotEmpty,
-	ArrayMinSize,
-	IsPositive,
-	MinLength,
-	MaxLength,
+	IsDate,
+	IsBoolean,
 } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { SubscriptionStatus } from '../schemas/subscription.schema';
 
-export class CreateSubscriptionDto {}
+export class CreateSubscriptionDto {
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	accountID: string;
+
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	billItemID: string;
+
+	@ApiProperty()
+	@IsDate()
+	expires: Date;
+
+	@ApiProperty()
+	@IsBoolean()
+	renew?: boolean;
+
+	@ApiProperty()
+	@IsEnum(SubscriptionStatus)
+	status?: SubscriptionStatus;
+}
