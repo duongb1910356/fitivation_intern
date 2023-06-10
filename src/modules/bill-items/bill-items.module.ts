38dd27aef7f35ff3d5adaa24c3e67350e4cb7638
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-10 11:44:21 +0700

    feat(bill-item module): connect bill-item schema in its module

--- src/modules/bill-items/bill-items.module.ts
+++ src/modules/bill-items/bill-items.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BillItemsController } from './bill-items.controller';
 import { BillItemsService } from './bill-items.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BillItem, BillItemSchema } from './schemas/bill-item.schema';
 
 @Module({
+	imports: [
+		MongooseModule.forFeature([
+			{ name: BillItem.name, schema: BillItemSchema },
+		]),
+	],
 	controllers: [BillItemsController],
 	providers: [BillItemsService],
 })
