fe7c81a3eda53713cb05f1d7ca409dad6dfee244
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-10 11:59:56 +0700

    fix(bill-item controller): remove Post module

--- src/modules/bill-items/bill-items.controller.ts
+++ src/modules/bill-items/bill-items.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { BillItemsService } from './bill-items.service';
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BillItem, BillItemStatus } from './schemas/bill-item.schema';
