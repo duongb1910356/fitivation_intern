b7389f9ab7ae813f20c1b01ae9a384dafd8c3b99
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-10 11:47:36 +0700

    style(bill-item.controller.spec): fix esilnt error

--- src/modules/bill-items/bill-items.controller.spec.ts
+++ src/modules/bill-items/bill-items.controller.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BillItemsController } from './bill-items.controller';
 
 describe('BillItemsController', () => {
-  let controller: BillItemsController;
+	let controller: BillItemsController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [BillItemsController],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [BillItemsController],
+		}).compile();
 
-    controller = module.get<BillItemsController>(BillItemsController);
-  });
+		controller = module.get<BillItemsController>(BillItemsController);
+	});
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(controller).toBeDefined();
+	});
 });
