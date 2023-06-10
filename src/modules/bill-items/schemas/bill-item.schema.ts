ee38d40a5de6af90c34411fd61a65e371ddf6c84
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-10 11:46:09 +0700

    refactor(bill-item schema): seperate some prop into new schema

--- src/modules/bill-items/schemas/bill-item.schema.ts
+++ src/modules/bill-items/schemas/bill-item.schema.ts
@@ -3,12 +3,14 @@ import mongoose, { HydratedDocument } from 'mongoose';
 import { Brand } from 'src/modules/brand/schemas/brand.schema';
 import { Facility } from 'src/modules/facility/schemas/facility.schema';
 import { PackageType } from 'src/modules/package-type/entities/package-type.entity';
-import { Package, TimeType } from 'src/modules/package/entities/package.entity';
+import { Package } from 'src/modules/package/entities/package.entity';
 import {
 	Promotion,
 	PromotionSchema,
 } from 'src/modules/promotions/schemas/promotion.schema';
 import { BaseObject } from 'src/shared/schemas/base-object.schema';
+import { BillItemFacility } from './bill-item-facility.schema';
+import { BillItemPackage } from './bill-item-package.schema';
 
 export type BillItemsDocument = HydratedDocument<BillItem>;
 
@@ -19,66 +21,50 @@ export enum BillItemStatus {
 
 @Schema({ timestamps: true })
 export class BillItem extends BaseObject {
-	@Prop({
-		type: {
-			facilityID: { type: mongoose.Schema.Types.ObjectId, ref: 'Facility' },
-			brandID: { type: mongoose.Schema.Types.ObjectId, ref: 'Brand' },
-			brandName: { type: String },
-			ownerFacilityName: { type: String },
-			facilityName: { type: String },
-			facilityAddress: { type: Object },
-			facilityCoordinatesLocation: {
-				type: [Number],
-				required: true,
-				default: [],
-			},
-			facilityPhoto: { default: '', type: String },
-		},
-	})
-	facilityInfo: {
-		facilityID: Facility;
-		brandID: Brand;
-		brandName: string;
-		ownerFacilityName: string;
-		facilityName: string;
-		facilityAddress: object;
-		facilityCoordinatesLocation: [number, number];
-		facilityPhoto: string;
-	};
+	@Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Brand' })
+	brandID: Brand;
 
 	@Prop({
-		type: {
-			packageTypeID: {
-				type: mongoose.Schema.Types.ObjectId,
-				ref: 'PackageType',
-			},
-			name: { type: String },
-			description: { type: String },
-			price: { type: Number, min: 0 },
-		},
+		required: true,
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Facility',
 	})
-	packageTypeInfo: {
-		packageTypeID: PackageType;
-		name: string;
-		desctiption?: string;
-		price: number;
-	};
+	facilityID: Facility;
 
 	@Prop({
-		type: {
-			packageID: {
-				type: mongoose.Schema.Types.ObjectId,
-				ref: 'Package',
-			},
-			type: { enum: TimeType, type: String },
-			price: { type: Number, min: 0 },
-		},
+		required: true,
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'PackageType',
 	})
-	packageInfo: {
-		packageID: Package;
-		type: TimeType;
-		price: number;
-	};
+	packageTypeID: PackageType;
+
+	@Prop({
+		required: true,
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Package',
+	})
+	packageID: Package;
+
+	@Prop({
+		required: true,
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'BillItemFacility',
+	})
+	facilityInfor: BillItemFacility;
+
+	@Prop({
+		required: true,
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'PackageType',
+	})
+	packageTypeInfo: PackageType;
+
+	@Prop({
+		required: true,
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'BillItemPackage',
+	})
+	packageInfo: BillItemPackage;
 
 	@Prop({ type: [PromotionSchema] })
 	promotions?: Promotion[];
