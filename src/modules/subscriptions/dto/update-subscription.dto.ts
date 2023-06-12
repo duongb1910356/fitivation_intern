c66d1d233d74e7dcfc41a070bb074aa43fe50a9b
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-12 14:08:51 +0700

    fix(update-subscription.dto): add expires prop

--- src/modules/subscriptions/dto/update-subscription.dto.ts
+++ src/modules/subscriptions/dto/update-subscription.dto.ts
@@ -1,6 +1,11 @@
-import { IsBoolean } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsBoolean, IsDate } from 'class-validator';
 
 export class UpdateSubscriptionDto {
 	@IsBoolean()
 	renew: boolean;
+
+	@IsDate()
+	@Type(() => Date)
+	expires: Date;
 }
