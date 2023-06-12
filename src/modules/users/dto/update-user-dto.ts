f940627cbd8b04005f18e285bc41f0a0a55a4137
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-12 15:46:43 +0700

    fix(update-user-dto.ts): add prop: status, username, password, tel, address. Remove: avatar prop

--- src/modules/users/dto/update-user-dto.ts
+++ src/modules/users/dto/update-user-dto.ts
@@ -1,18 +1,24 @@
 import { PickType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user-dto';
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsString } from 'class-validator';
 import { UserStatus } from '../schemas/user.schema';
 
 export class UpdateUserDto extends PickType(CreateUserDto, [
+	'username',
 	'email',
+	'password',
 	'displayName',
 	'lastName',
 	'firstName',
 	'birthDate',
-	'avatar',
 	'gender',
 	'isMember',
+	'tel',
+	'address',
 ]) {
+	@IsString()
+	id: string;
+
 	@IsEnum(UserStatus)
 	status: UserStatus;
 }
