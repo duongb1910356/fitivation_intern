64e97669eed885c24dbfb30af5f95e3288d0369a
DustHuynh <huynhdust0411@gmail.com>
2023-06-17 16:50:50 +0700

    fix(admin): import module and service to admin

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
+import { PackageTypeModule } from '../package-type/package-type.module';
+import { PackageModule } from '../package/package.module';
+import { FacilityCategoryModule } from '../facility-category/facility-category.module';
 
 @Module({
+	imports: [PackageTypeModule, PackageModule, FacilityCategoryModule],
 	controllers: [AdminController],
 })
 export class AdminModule {}
