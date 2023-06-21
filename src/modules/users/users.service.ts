5a85921a003c9b19e9bb89b5fa06c602d9f38a7e
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-21 14:37:37 +0700

    feat(user service): build checkExist & createOne service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,13 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user-dto';
+import { SignupDto } from '../auth/dto/signup-dto';
 
 @Injectable()
 export class UsersService {
@@ -16,8 +22,17 @@ export class UsersService {
 	updateOne() {
 		return 'updateOne';
 	}
-	createOne() {
-		return 'createOne';
+	async createOne(dto: CreateUserDto | SignupDto): Promise<User> {
+		const isExist = await this.checkExist({
+			email: dto.email,
+			username: dto.username,
+		});
+
+		if (isExist.value) throw new BadRequestException(isExist.message);
+
+		const user = await this.userModel.create(dto);
+		user.refreshToken = undefined;
+		return user;
 	}
 	deleteOne() {
 		return 'deleteOne';
@@ -37,5 +52,38 @@ export class UsersService {
 		} catch (err) {
 			throw new InternalServerErrorException(err);
 		}
+	}
+
+	async checkExist(uniqueFieldObj: {
+		username: string;
+		email: string;
+	}): Promise<{ value: boolean; message: string }> {
+		const isEmailExisted =
+			(await this.userModel.exists({ email: uniqueFieldObj.email })) === null
+				? false
+				: true;
+
+		if (isEmailExisted)
+			return {
+				value: true,
+				message: 'Email already exists',
+			};
+
+		const isUsernameExisted =
+			(await this.userModel.exists({ username: uniqueFieldObj.username })) ===
+			null
+				? false
+				: true;
+
+		if (isUsernameExisted)
+			return {
+				value: true,
+				message: 'Username already exists',
+			};
+
+		return {
+			value: false,
+			message: null,
+		};
 	}
 }
