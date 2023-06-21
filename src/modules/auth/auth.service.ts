acf0c92f572b389985f877aa2b6b4eacc47a3282
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-21 15:02:33 +0700

    feat(auth service): build signTokens service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { SignupDto } from './dto/signup-dto';
+import { TokenResponse } from './types/token-response.types';
+import { TokenPayload } from './types/token-payload.type';
+import { JwtService } from '@nestjs/jwt';
+import { appConfig } from '../../app.config';
 
 @Injectable()
 export class AuthService {
-	constructor(private readonly userService: UsersService) {}
+	constructor(
+		private userService: UsersService,
+		private jwtService: JwtService,
+	) {}
+
+	async signTokens(
+		sub: string,
+		email: string,
+		role: string,
+	): Promise<TokenResponse> {
+		const tokenPayload: TokenPayload = {
+			sub,
+			email,
+			role,
+		};
+
+		const [at, rt] = await Promise.all([
+			this.jwtService.signAsync(tokenPayload, {
+				secret: appConfig.jwtAccessSecret,
+				expiresIn: appConfig.jwtAccessExpiresIn,
+			}),
+			this.jwtService.signAsync(tokenPayload, {
+				secret: appConfig.jwtRefreshSecret,
+				expiresIn: appConfig.jwtRefreshExpiresIn,
+			}),
+		]);
+
+		return {
+			accessToken: at,
+			refreshToken: rt,
+		};
+	}
 
 	async signup(signupDto: SignupDto) {
 		//
