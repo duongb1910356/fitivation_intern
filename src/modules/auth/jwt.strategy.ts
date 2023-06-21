c8aa3e5b509b50924831f5805a8569be77f27f25
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-21 15:31:34 +0700

    fix(jwt.strategy): fix unread secret and update JwtStrategy name

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -5,12 +5,12 @@ import { appConfig } from '../../app.config';
 import { TokenPayload } from './types/token-payload.type';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 	constructor() {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
-			secretOrKey: appConfig.jwtAccessSecret,
+			secretOrKey: `${appConfig.jwtAccessSecret}`,
 		});
 	}
 
