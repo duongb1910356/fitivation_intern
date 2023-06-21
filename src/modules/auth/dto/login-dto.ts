5f1e9e35e664d3c69560928f6af3af9bde348517
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-21 19:34:38 +0700

    build(auth dto): add login-dto

--- src/modules/auth/dto/login-dto.ts
+++ src/modules/auth/dto/login-dto.ts
@@ -1,1 +1,7 @@
-export class LoginDto {}
+import { CreateUserDto } from 'src/modules/users/dto/create-user-dto';
+import { PickType } from '@nestjs/swagger';
+
+export class LoginDto extends PickType(CreateUserDto, [
+	'email',
+	'password',
+] as const) {}
