f3779ef3025383c6ccb8c620f34b90fb805026a9
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-21 15:15:19 +0700

    fix(update-user-dto): rebuild

--- src/modules/users/dto/update-user-dto.ts
+++ src/modules/users/dto/update-user-dto.ts
@@ -1,28 +1,9 @@
-import { PickType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user-dto';
-import { IsEnum, IsString } from 'class-validator';
-import { UserStatus } from '../schemas/user.schema';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PickType(CreateUserDto, [
-	'username',
-	'email',
-	'password',
-	'displayName',
-	'lastName',
-	'firstName',
-	'birthDate',
-	'gender',
-	'isMember',
-	'tel',
-	'address',
-]) {
-	@IsString()
-	id: string;
-
+export class UpdateUserDto extends PartialType(CreateUserDto) {
 	@IsOptional()
 	@IsString()
 	refreshToken?: string;
-
-	@IsEnum(UserStatus)
-	status: UserStatus;
 }
