5a91d3a505fc655ebcdf41f95d3796a3d9a2f086
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-21 14:52:18 +0700

    build(auth dto): add signup-dto

--- src/modules/auth/dto/signup-dto.ts
+++ src/modules/auth/dto/signup-dto.ts
@@ -1,1 +1,6 @@
-export class SignupDto {}
+import { CreateUserDto } from 'src/modules/users/dto/create-user-dto';
+import { PartialType, OmitType } from '@nestjs/swagger';
+
+export class SignupDto extends PartialType(
+	OmitType(CreateUserDto, ['role', 'isMember', 'status'] as const),
+) {}
