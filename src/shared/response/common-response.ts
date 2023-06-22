299ae9907c4ac493d165e8d4347ecb3e8d85d43e
dust <dusthuynh.dev@gmail.com>
2023-06-22 16:42:18 +0700

    fix(common-response): restore before vesion

--- src/shared/response/common-response.ts
+++ src/shared/response/common-response.ts
@@ -1,14 +1,9 @@
-type Projection<T> = {
-	[K in keyof T]?: 1 | 0;
-};
-
 export type ListOptions<T = any> = Partial<T> & {
 	limit?: number;
 	offset?: number;
 	search?: string;
 	sortField?: keyof T;
 	sortOrder?: 'asc' | 'desc';
-	projection?: Projection<T>;
 };
 
 export interface ListResponse<T> {
