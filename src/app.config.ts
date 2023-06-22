4cafb1d556d104c761cb78629062eb25654f9cfa
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-22 10:59:27 +0700

    build(.env, .yml, app.config): update env

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,6 +2,8 @@ export const appConfig = {
 	name: process.env.APP_NAME,
 	version: process.env.APP_VERSION,
 	mongoURI: process.env.MONGO_URI,
+	fileHost: process.env.FILE_HOST,
+	fileRoot: process.env.FILE_ROOT,
 	jwtAccessSecret: process.env.JWT_AT_SECRET,
 	jwtAccessExpiresIn: process.env.JWT_AT_EXPIRES_IN,
 	jwtRefreshSecret: process.env.JWT_RT_SECRET,
