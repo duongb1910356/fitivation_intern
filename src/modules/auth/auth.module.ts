b493fb30a98294ac1fc435adaba14d12f5577512
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-22 11:25:14 +0700

    refactor(jwt strategy, jwt guard): update file path

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../../modules/users/users.module';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthController } from './auth.controller';
 
 @Module({
