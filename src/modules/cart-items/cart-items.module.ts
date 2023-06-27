703f9faa7a9dc1eb10e907fd8b3f5afe835aaa3a
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-27 10:08:06 +0700

    feat(cart-item): import model

--- src/modules/cart-items/cart-items.module.ts
+++ src/modules/cart-items/cart-items.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CartItemsController } from './cart-items.controller';
 import { CartItemsService } from './cart-items.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CartItem, CartItemSchema } from './schemas/cart-item.schema';
 
 @Module({
+	imports: [
+		MongooseModule.forFeature([
+			{ name: CartItem.name, schema: CartItemSchema },
+		]),
+	],
 	controllers: [CartItemsController],
 	providers: [CartItemsService],
 	exports: [CartItemsService],
--- src/modules/cart-items/cart-items.service.ts
+++ src/modules/cart-items/cart-items.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CartItem, CartItemDocument } from './schemas/cart-item.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class CartItemsService {}
+export class CartItemsService {
+	constructor(
+		@InjectModel(CartItem.name) private cartItemModel: Model<CartItemDocument>,
+	) {}
+}
