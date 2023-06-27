2aa23a67f47c1dd953b898a1c25380318a5836b7
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-27 20:13:29 +0700

    feat(cart service): fix getCurrent, add deleteOne, add addCartItemToCurrentCart, removeCartItemFromCurrentCart

--- src/modules/carts/carts.service.ts
+++ src/modules/carts/carts.service.ts
@@ -20,32 +20,88 @@ export class CartsService {
 	}
 
 	async getCurrent(userID: string): Promise<Cart> {
-		const cart = await this.cartModel.findOne({ accountID: userID });
+		const cart = await this.cartModel
+			.findOne({ accountID: userID })
+			.populate({ path: 'cartItemIDs' });
 
-		if (!cart) throw new BadRequestException(`Not found user's cart`);
+		if (!cart) throw new BadRequestException(`Not found current user's cart`);
 
 		return cart;
 	}
 
 	async deleteOne(userID: string): Promise<boolean> {
+		const cart = await this.cartModel.findOne({ accountID: userID });
+
+		if (!cart) throw new BadRequestException(`Not found current user's cart`);
+
+		const cartItems = cart.cartItemIDs;
+
+		cartItems.forEach(async (el) => await this.cartItemService.deleteOne(el));
+
 		await this.cartModel.deleteOne({ accountID: userID });
 
 		return true;
 	}
 
-	async addCartItemToCurrentCart(userID: string, packageID: string) {
-		await this.packageService.findOneByID(packageID);
+	async addCartItemToCurrentCart(
+		userID: string,
+		packageID: string,
+	): Promise<boolean> {
+		const cart = await this.cartModel
+			.findOne({ accountID: userID })
+			.populate({ path: 'cartItemIDs' });
+
+		if (!cart) throw new BadRequestException(`Not found current user's cart`);
+
+		const isDuplicateProduct = this.checkDuplicateCartItemProduct(
+			cart.cartItemIDs,
+			packageID,
+		);
+
+		if (isDuplicateProduct)
+			throw new BadRequestException('Duplicate Product in cart');
 
 		const cartItem = await this.cartItemService.createCartItem(packageID);
 
-		const cart = await this.cartModel.findOne({ accountID: userID });
+		cart.cartItemIDs.push(cartItem._id);
 
-		cart.promotionIDs.push(cartItem._id);
+		cart.totalPrice += cartItem.totalPrice;
 
 		cart.save();
+
+		return true;
 	}
 
-	async removeCartItemToCurrentCart(userID: string, packageID) {
-		//
+	async removeCartItemFromCurrentCart(
+		userID: string,
+		cartItemID: string,
+	): Promise<boolean> {
+		const cartItem = await this.cartItemService.findOne(cartItemID);
+
+		const cart = await this.cartModel
+			.findOneAndUpdate(
+				{ accountID: userID },
+				{ $pull: { cartItemIDs: { $in: cartItemID } } },
+			)
+			.populate({ path: 'cartItemIDs' });
+
+		if (!cart) throw new BadRequestException(`Not found current user's cart`);
+		console.log(cart.totalPrice, cartItem.totalPrice);
+
+		cart.totalPrice -= cartItem.totalPrice;
+
+		cart.save();
+
+		await this.cartItemService.deleteOne(cartItemID);
+
+		return true;
+	}
+
+	checkDuplicateCartItemProduct(cartItems: any[], productID: string): boolean {
+		let isDuplicate = false;
+		for (let i = 0; i < cartItems.length; i++) {
+			if (cartItems[i].packageID.toString() === productID) isDuplicate = true;
+		}
+		return isDuplicate;
 	}
 }
