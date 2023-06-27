b11bb19a018ced7d15df09558769a1cf84ec66e3
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-27 10:02:38 +0700

    feat(cart): build createCart, getCurrent, deleteOne services

--- src/modules/carts/carts.service.ts
+++ src/modules/carts/carts.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Cart, CartDocument } from './schemas/cart.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class CartsService {
-	constructor(@InjectModel(Cart.name) private userModel: Model<CartDocument>) {}
+	constructor(@InjectModel(Cart.name) private cartModel: Model<CartDocument>) {}
+
+	async createCart(userID: string): Promise<Cart> {
+		const cart = await this.cartModel.create({ accountID: userID });
+
+		return cart;
+	}
+
+	async getCurrent(userID: string): Promise<Cart> {
+		const cart = await this.cartModel.findOne({ accountID: userID });
+
+		if (!cart) throw new BadRequestException(`Not found user's cart`);
+
+		return cart;
+	}
+
+	async deleteOne(userID: string): Promise<boolean> {
+		await this.cartModel.deleteOne({ accountID: userID });
+
+		return true;
+	}
 }
