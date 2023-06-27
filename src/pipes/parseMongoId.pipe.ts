60d06c1b155adf3d23b3292d00223dfe845d5388
dust <dusthuynh.dev@gmail.com>
2023-06-27 16:10:19 +0700

    fix(MongoIdPipe): delete console.log

--- src/pipes/parseMongoId.pipe.ts
+++ src/pipes/parseMongoId.pipe.ts
@@ -9,7 +9,6 @@ import { Types } from 'mongoose';
 @Injectable()
 export class MongoIdValidationPipe implements PipeTransform {
 	transform(value: any, metadata: ArgumentMetadata) {
-		console.log(value, metadata);
 		if (!Types.ObjectId.isValid(value)) {
 			throw new BadRequestException(`Invalid ${metadata.data}`);
 		}
