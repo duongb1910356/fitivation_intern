f1168bdffcdfd148e22eb64586b8bd71ed33f89a
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-27 20:27:43 +0700

    build(cart-item module): import pacakgeModule

--- src/modules/cart-items/cart-items.module.ts
+++ src/modules/cart-items/cart-items.module.ts
@@ -3,12 +3,14 @@ import { CartItemsController } from './cart-items.controller';
 import { CartItemsService } from './cart-items.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CartItem, CartItemSchema } from './schemas/cart-item.schema';
+import { PackageModule } from '../package/package.module';
 
 @Module({
 	imports: [
 		MongooseModule.forFeature([
 			{ name: CartItem.name, schema: CartItemSchema },
 		]),
+		PackageModule,
 	],
 	controllers: [CartItemsController],
 	providers: [CartItemsService],
