55b495302bca9692f9f15057f167e3fed665db0f
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-28 16:02:49 +0700

    feat(bill service): add findMany, findOneByID

--- src/modules/bills/bills.service.ts
+++ src/modules/bills/bills.service.ts
@@ -1,8 +1,19 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+	ForbiddenException,
+	Injectable,
+	InternalServerErrorException,
+} from '@nestjs/common';
 import { BillItem } from '../bill-items/schemas/bill-item.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Bill, BillDocument, PaymentMethod } from './schemas/bill.schema';
 import { Model } from 'mongoose';
+import {
+	ListResponse,
+	QueryAPI,
+	QueryObject,
+} from 'src/shared/utils/query-api';
+import { UserRole } from '../users/schemas/user.schema';
+import { TokenPayload } from '../auth/types/token-payload.type';
 
 @Injectable()
 export class BillsService {
@@ -38,6 +49,40 @@ export class BillsService {
 		if (!bill)
 			throw new InternalServerErrorException('Create Bill-item failed');
 
+		return bill;
+	}
+
+	async findMany(
+		query: QueryObject,
+		user: TokenPayload,
+	): Promise<ListResponse<Bill>> {
+		const queryFeatures = new QueryAPI(this.billModel, query)
+			.filter()
+			.sort()
+			.limitfields()
+			.paginate();
+
+		if (user.role === UserRole.MEMBER) {
+			queryFeatures.queryModel.find({ accountID: user.sub });
+		}
+
+		const bills = await queryFeatures.queryModel;
+
+		return {
+			total: bills.length,
+			queryOptions: queryFeatures.queryOptions,
+			items: bills,
+		};
+	}
+
+	async findOneByID(billID: string, user: TokenPayload): Promise<Bill> {
+		const bill = await this.billModel.findById(billID);
+		if (
+			user.sub.toString() !== bill.accountID.toString() &&
+			user.role === UserRole.MEMBER
+		) {
+			throw new ForbiddenException('Forbidden resource');
+		}
 		return bill;
 	}
 }
