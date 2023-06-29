70ec6d21896504db9343a8a202209e11783e732c
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-06-29 16:09:15 +0700

    fix(bill-items): update findOneByID

--- src/modules/bill-items/bill-items.service.ts
+++ src/modules/bill-items/bill-items.service.ts
@@ -108,9 +108,7 @@ export class BillItemsService {
 		const billItem = await this.billItemsModel.findById(billItemId);
 
 		const bill = await this.BillService.findOne({
-			billItems: {
-				_id: billItem._id.toString(),
-			},
+			billItems: { $in: billItem },
 		});
 
 		if (
