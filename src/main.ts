18c19354d8483f057128ac442f441357069c3adb
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-07-06 14:10:57 +0700

    fix(main): import cookieParser

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { appConfig } from './app.config';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
