09dfaeedb678d296a213f0df8a540e797f5d2ebe
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-07-17 16:19:50 +0700

    fix(payment): update payment-response type

--- src/modules/payments/types/payment-response.type.ts
+++ src/modules/payments/types/payment-response.type.ts
@@ -1,8 +1,10 @@
 import { Bill } from 'src/modules/bills/schemas/bill.schema';
+import { Subscription } from 'src/modules/subscriptions/schemas/subscription.schema';
 
 export class PaymentResponse {
 	message?: string;
 	clientSecret: string;
 	paymentIntentID: string;
-	bill: Bill;
+	bill?: Bill;
+	subscription?: Subscription;
 }
