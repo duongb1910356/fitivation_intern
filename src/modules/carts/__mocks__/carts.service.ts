d79dcec5edf012af26600b8ea00e4fe3be59e160
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-07-25 15:55:59 +0700

    fix(cart): delete carts.service mock

--- src/modules/carts/__mocks__/carts.service.ts
+++
@@ -1,7 +0,0 @@
-import { cartStub } from '../test/stubs/cart.stub';
-
-export const CartsService = jest.fn().mockReturnValue({
-	getCurrent: jest.fn().mockResolvedValue(cartStub()),
-	addCartItemToCurrentCart: jest.fn().mockResolvedValue(true),
-	removeCartItemFromCurrentCart: jest.fn().mockResolvedValue(true),
-});
