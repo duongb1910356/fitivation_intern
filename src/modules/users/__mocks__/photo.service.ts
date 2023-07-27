5c83ac396d054d203c75b765cbbf1cb45188644b
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-07-27 11:20:02 +0700

    refactor(mock cart-item, cart, photo): move other folder and delete

--- src/modules/users/__mocks__/photo.service.ts
+++
@@ -1,6 +0,0 @@
-import { PhotoStub } from '../test/stubs/photo.stub';
-
-export const PhotoService = jest.fn().mockReturnValue({
-	delete: jest.fn().mockResolvedValue(PhotoStub()),
-	uploadOneFile: jest.fn().mockResolvedValue(PhotoStub()),
-});
