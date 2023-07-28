012f469d229a2fef9df4f04207093d58f234b752
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-07-28 14:33:51 +0700

    test(payment): build stub

---
+++ src/modules/bill-items/test/stubs/bill-item.stub.ts
@@ -0,0 +1,99 @@
+export const billItemStub = (): any => {
+	return {
+		_id: '64b5fb7409c136b1b2789dc6',
+		brandID: '64944c7c2d7cf0ec0dbb4051',
+		facilityID: '649d344f72e91c40d2e7e895',
+		packageTypeID: '6476ef7d1f0419cd330fe681',
+		packageID: '649dd2e7e895344f72e91c42',
+		ownerFacilityID: '6497c6807a114f5b35a393fd',
+		accountID: '649a8f8ab185ffb672485391',
+		facilityInfo: {
+			brandName: 'TheHinhOnline 1',
+			facilityName: 'Gym Thái Sơn',
+			facilityAddress: {
+				street: '54 Hùng Vương',
+				province: 'Cần Thơ',
+				provinceCode: '065',
+				district: 'Ninh Kiều',
+				districtCode: '067',
+				commune: 'An Hội',
+				communeCode: '066',
+			},
+			facilityCoordinatesLocation: {
+				type: 'Point',
+				coordinates: [105.77827419395031, 10.044071865857335],
+			},
+			facilityPhotos: [
+				{
+					createdAt: new Date('2023-06-29T07:36:22.758Z'),
+					updatedAt: new Date('2023-06-29T07:36:22.758Z'),
+					ownerID: '649d344f72e91c40d2e7e895',
+					name: '1688024182737-366333986.jpeg',
+					_id: '649d347672e91c40d2e7e89c',
+					imageURL:
+						'http://localhost:8080/649d344f72e91c40d2e7e895/1688024182737-366333986.jpeg',
+				},
+				{
+					createdAt: new Date('2023-06-29T07:36:22.758Z'),
+					updatedAt: new Date('2023-06-29T07:36:22.758Z'),
+					ownerID: '649d344f72e91c40d2e7e895',
+					name: '1688024182746-73042410.png',
+					_id: '649d347672e91c40d2e7e89d',
+					imageURL:
+						'http://localhost:8080/649d344f72e91c40d2e7e895/1688024182746-73042410.png',
+				},
+				{
+					createdAt: new Date('2023-06-29T07:36:22.758Z'),
+					updatedAt: new Date('2023-06-29T07:36:22.758Z'),
+					ownerID: '649d344f72e91c40d2e7e895',
+					name: '1688024182724-411896153.png',
+					_id: '649d347672e91c40d2e7e89b',
+					imageURL:
+						'http://localhost:8080/649d344f72e91c40d2e7e895/1688024182724-411896153.png',
+				},
+				{
+					createdAt: new Date('2023-06-29T07:36:22.758Z'),
+					updatedAt: new Date('2023-06-29T07:36:22.758Z'),
+					ownerID: '649d344f72e91c40d2e7e895',
+					name: '1688024182696-205093289.png',
+					_id: '649d347672e91c40d2e7e899',
+					imageURL:
+						'http://localhost:8080/649d344f72e91c40d2e7e895/1688024182696-205093289.png',
+				},
+				{
+					createdAt: new Date('2023-06-29T07:36:22.758Z'),
+					updatedAt: new Date('2023-06-29T07:36:22.758Z'),
+					ownerID: '649d344f72e91c40d2e7e895',
+					name: '1688024182709-1336106.png',
+					_id: '649d347672e91c40d2e7e89a',
+					imageURL:
+						'http://localhost:8080/649d344f72e91c40d2e7e895/1688024182709-1336106.png',
+				},
+			],
+			_id: '64b5fb7409c136b1b2789dc7',
+			createdAt: new Date('2023-07-18T02:39:48.041Z'),
+			updatedAt: new Date('2023-07-18T02:39:48.041Z'),
+		},
+		packageTypeInfo: {
+			name: 'GYM GYM',
+			description: 'Các bài tập Gym có PT',
+			price: 140000,
+			_id: '64b5fb7409c136b1b2789dcd',
+			createdAt: new Date('2023-07-18T02:39:48.041Z'),
+			updatedAt: new Date('2023-07-18T02:39:48.041Z'),
+		},
+		packageInfo: {
+			type: '2',
+			price: 280000,
+			_id: '64b5fb7409c136b1b2789dce',
+			createdAt: new Date('2023-07-18T02:39:48.041Z'),
+			updatedAt: new Date('2023-07-18T02:39:48.041Z'),
+		},
+		promotions: [],
+		promotionPrice: 0,
+		totalPrice: 280000,
+		status: 'ACTIVE',
+		createdAt: new Date('2023-07-18T02:39:48.041Z'),
+		updatedAt: new Date('2023-07-18T02:39:48.041Z'),
+	};
+};
