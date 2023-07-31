0cc63aab1e3c115f71817092ae15ab15a123780c
duongb1910356 <duongb1910356@student.ctu.edu.vn>
2023-07-31 14:41:37 +0700

    Seed(cart): add seed cart

---
+++ src/seeder/cart-seeder.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
+import { Seeder } from 'nestjs-seeder';
+import { Cart } from 'src/modules/carts/schemas/cart.schema';
+import { cartData } from './data/cart-data';
+
+@Injectable()
+export class CartSeeder implements Seeder {
+	constructor(
+		@InjectModel(Cart.name)
+		private readonly cartModel: Model<Cart>,
+	) {}
+
+	async seed(): Promise<any> {
+		this.cartModel.insertMany(cartData);
+	}
+
+	async drop(): Promise<any> {
+		await this.cartModel.deleteMany({});
+	}
+}
